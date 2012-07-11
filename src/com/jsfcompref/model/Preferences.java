26c5c30626a06d0f8850cfa444a5a9810e946efa
vagner.oliveirars <vagner.oliveirars@gmail.com>
2012-07-11 13:02:16 +0000

    Adicionado Current Weight para testar o converter.

---
+++ src/com/jsfcompref/model/Preferences.java
@@ -0,0 +1,15 @@
+package com.jsfcompref.model;
+
+public class Preferences {
+
+	private Integer weightUnits;
+
+	public Integer getWeightUnits() {
+		return weightUnits;
+	}
+
+	public void setWeightUnits(Integer weightUnits) {
+		this.weightUnits = weightUnits;
+	}
+
+}
